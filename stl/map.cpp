b2e407f5f4b5f2bf9af91cc6bca3ff4e1eae84a9
wilson <wiljm0.0@gmail.com>
2014-08-08 15:31:47 -0500

    add map to stl

---
+++ stl/map.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(){
+	map<string, int> m;
+	m["hola"]=3;
+	m["como"]=2;
+	m["estas"]=5;
+	
+	map<string, int>::iterator it;
+	
+	for(it=m.begin(); it!=m.end(); it++){
+		//cout<<(*it).first<<": "<<(*it).second<<endl;
+		cout<<it->first<<" "<<it->second<<endl;
+	}
+	
+}
+
