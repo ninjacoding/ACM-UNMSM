025d1c227d848db1866be9e38436eb921af71dbc
wilson <wiljm0.0@gmail.com>
2014-08-10 14:27:45 -0500

    refactoring

--- problems/names.cpp
+++ problems/names.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <set>
+
 using namespace std;
 
 string minuscula1(string x){
 	string y="";
 	for(int i=0 ;i<(int)x.size(); i++)
 		y+=tolower(x[i]);
-	return y;
-}
-string minuscula2(string x){
-	string y="";
-	for(int i=0 ;i<(int)x.size(); i++)
-		if (x[i]>'A' || x[i]<'Z') y+=x[i]+32;
-		else y+=x[i];	
-	cout<<y<<endl;
 	return y;
 }
 
--- problems/paretheses balance.cpp
+++ problems/paretheses balance.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+
 using namespace std;
 
 bool balanced(string cadena){
