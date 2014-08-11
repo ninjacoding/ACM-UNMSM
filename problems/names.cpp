056463eef1a47cf9b89cc0c0a00897a7c01701f2
wilson <wiljm0.0@gmail.com>
2014-08-11 00:07:01 -0500

    add other method

--- problems/names.cpp
+++ problems/names.cpp
@@ -10,6 +10,12 @@ string minuscula1(string x){
 	return y;
 }
 
+string minuscula2(string x){
+	for(int i=0 ;i<(int)x.size(); i++)
+		if(!(x[i]&32)) x[i]+=32;
+	return x;
+}
+
 int main(){
 	int n;
 	string x;
@@ -17,7 +23,7 @@ int main(){
 		set <string> s;
 		for(int i=0;i<n;i++){
 			cin>>x;
-			s.insert(minuscula1(x));
+			s.insert(minuscula2(x));
 		}
 		cout<<s.size()<<endl;
 	}
