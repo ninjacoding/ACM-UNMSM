73c4bab2c26c5f81bd127213f33f93fe2cdee34b
gianco409 <gianco409@gmail.com>
2014-09-11 19:13:56 -0500

    Update criba.cpp

--- algorithms/criba.cpp
+++ algorithms/criba.cpp
@@ -1,21 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <cstdio>
+#define N 20000000
+
 using namespace std;
 
+bool v[N];
+
+//CRIBA DE ERATOSTENES PARA LOS NUMEROS PRIMOS
+void criba (){
+    memset (v, true, sizeof (v));
+    v [0] = v [1] = false;
+ 
+    for ( int i = 4; i < N; i += 2 )
+        v [i] = false;
+ 
+    for ( int i = 3; i * i <= N; i++ ) {
+        if ( v [i] ) {
+            for ( int j = i * i; j < N; j += 2 * i )
+                v [j] = false;
+        }
+    }
+}
+
 int main(){
-   vector<bool> v(100000,true);
-   for(int i = 2; i<100000; ++i){
-      if(v[i]){
-         //marcamos los demas numeros.
-         for(int j = i*2;j<100000;j += i){
-            v[j] = false;   
-         }   
-      }   
-   }
    int x;
-   while(cin>>x){
-      if(v[x])cout<<"esPrimo"<<endl;
-      else cout<<"No es Primo"<<endl;   
-   }
    
+   criba();
+   
+   while(cin>>x){
+      if(v[x]) printf("esPrimo\n");
+      else printf("No es Primo\n");   
+   }
+   return 0;
 }
