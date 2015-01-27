ebbdc923572bd07e99fe9f743a789ba1a342ff89
wils0n <wiljm0.0@gmail.com>
2015-01-27 07:00:27 -0500

    upload first class of Introduction to Programming

---
+++ Verano 2015/Introduction to Programming/source/concadenar_cadenas.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+# la cadena de la primera línea me permite escribir caracteres especiales como tildes, etc
+
+a = "Hola"
+b = " Mundo"
+
+c = a + b
+
+print c
+
---
+++ Verano 2015/Introduction to Programming/source/suma_de_3_numeros.py
@@ -0,0 +1,12 @@
+# suma 3 valores, y esos valores que sean 
+# solicitados por teclado
+# guardarlos la suma de los valores en la variable d
+# mostrar la suma
+
+a = input("a = ")
+b = input("b = ")
+c = input("c = ")
+
+d = a + b + c
+
+print "la suma de a + b + c = ", d
