330fd693310777bf517c9d6713f3920a140081d9
wils0n <wiljm0.0@gmail.com>
2015-01-28 17:20:01 -0500

    Clase 2 Verano ACM UNMSM

---
+++ Verano 2015/Introduction to Programming/source/2. variables.py
@@ -0,0 +1,57 @@
+# variables - is like a box that holds a value
+# variables - placeholders for important values
+# used to recomputing values and to
+# give values names that help reader understand code
+
+# valid variable names - consists of letters, numbers, underscore (_)
+# starts with letter or underscore
+# case sensitive
+
+# legal names - ninja, Ninja, n_i_n_j_a
+# illegal names - 1337, 1337ninja
+
+# Python convention - multiple words joined by _
+# legal names - elite_ninja, ninja_1337
+# illegal names - 1337_ninja
+
+# assign to variable name using single equal sign =
+# (remember that double equals == is used to test equality)
+
+# examples
+
+my_name = "Wilson J. Mejia"
+print my_name
+
+my_age = 20
+print my_age
+
+# birthday - add one
+
+#my_age = 20 + 1
+#my_age = my_age + 1
+my_age +=  1
+print my_age
+
+
+# Temperature examples
+
+# convert from Fahrenheit to Celsius
+# c = 5 / 9 * (f - 32)
+# use explanatory names
+
+# test it! 32 Fahrenheit is 0 Celsius, 212 Fahrenheit is 100 Celsius
+temp_fahrenheit = 32  #change to 212
+temp_celsius = 5 / 9 * (temp_fahrenheit - 32)
+print temp_celsius
+
+
+# work for students
+# convert from Celsius to Fahrenheit
+
+# f = 9 / 5 *c + 32
+
+# test it! 0 Celsius is 32 Fahrenheit
+
+temp_celsius = 0  #change to 32
+temp_fahrenheit = 9 / 5 * temp_celsius + 32
+print temp_fahrenheit
--- Verano 2015/Introduction to Programming/source/concadenar_cadenas.py
+++ Verano 2015/Introduction to Programming/source/concadenar_cadenas.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# la cadena de la primera línea me permite escribir caracteres especiales como tildes, etc
+# la cadena de la primera línea me permite escribir caracteres especiales como tildes, ñ, etc
 
 a = "Hola"
 b = " Mundo"
