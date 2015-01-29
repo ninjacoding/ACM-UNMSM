b3d11131186538f88529b25052eb04a200b2393d
wils0n <wiljm0.0@gmail.com>
2015-01-29 18:30:32 -0500

    update class 2

---
+++ Verano 2015/Introduction to Programming/source/2.3 celsius_to_fahrenheit.py
@@ -0,0 +1,10 @@
+# work for students
+# convert from Celsius to Fahrenheit
+
+# f = 9 / 5 *c + 32
+
+# test it! 0 Celsius is 32 Fahrenheit
+
+temp_celsius = 0
+temp_fahrenheit = (9 / 5.0) * temp_celsius + 32
+print temp_fahrenheit
